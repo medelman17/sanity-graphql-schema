7357b79201e5679e5e16b57fc83a8422efa18b22
Michael Edelman <metalman@amazon.com>
2021-02-25 14:48:11 -0500

    fix: conform external type stubs to gql spec

--- src/stubExternalTypes.js
+++ src/stubExternalTypes.js
@@ -2,8 +2,9 @@ const {upperFirst} = require('lodash')
 
 module.exports = function stubExternalTypes(types) {
   return types.reduce((sdl, type) => {
+    const conformedTypeName = type.name.replace('-','')
     return `${sdl}
     # Stub to make external types work
-    scalar ${upperFirst(type.name)} # ${type.name}\n\n`.replace(/(\n+)\s*/g, '$1')
+    scalar ${upperFirst(conformedTypeName)} # ${conformedTypeName}\n\n`.replace(/(\n+)\s*/g, '$1')
   }, '')
 }
